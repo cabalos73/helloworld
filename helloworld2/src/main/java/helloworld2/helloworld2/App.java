01a937faaee3b3d90f6c080a27f99e9bf60b4ead
cabalos73 <cabalos73@yahoo.com>
2016-02-11 11:32:35 -0800

    test

---
+++ helloworld2/src/main/java/helloworld2/helloworld2/App.java
@@ -0,0 +1,13 @@
+package helloworld2.helloworld2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
